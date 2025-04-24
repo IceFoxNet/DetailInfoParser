4c5fca4cf90c0b4b577fd06d23cd2288de1e1850
IceFoxNet <icefoxnet@gmail.com>
2025-04-24 18:03:40 +0300

    Async Global Update

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import os
 
 while True:
     try:
-        from playwright.sync_api import sync_playwright, expect
-        from configparser import ConfigParser
-        import gspread, pathlib, requests
+        from playwright.async_api import async_playwright, Browser
+        import gspread, pathlib, requests, asyncio
+        from async_lru import alru_cache
     except ImportError as e:
         package = e.msg.split()[-1][1:-1]
         os.system(f'python -m pip install {package}')
@@ -12,21 +12,15 @@ while True:
         break
 
 dir = pathlib.Path(__file__).parent.resolve()
+rub = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').json()['Valute']['USD']['Value']
 
-config = ConfigParser()
-config.read(os.path.join(dir, 'config.ini'))
-sheet_url = config.get('parser', 'url')
-
-def main(start: int, end: int, setup: dict):
+async def main(start: int, end: int, setup: dict):
     
     print('Подготавливаем всё для работы')
     if start < 3: start = 3
-    cache = {}
 
     # ==> ПОДКЛЮЧЕНИЕ ГУГЛ-АККАУНТА
-    creds = setup.get('GoogleCredentials')
-    google_client = gspread.authorize(creds)
-    spreadsheet = google_client.open_by_url(sheet_url)
+    spreadsheet: gspread.spreadsheet.Spreadsheet = setup.get('DetailsSheet')
     sheet = spreadsheet.worksheet('Детали')
     
     # ==> СОЗДАНИЕ ТАБЛИЦЫ ПЕРЕВОДА ЦВЕТА В ID
@@ -43,52 +37,56 @@ def main(start: int, end: int, setup: dict):
     price_res = []
     name_res = []
 
-    # ==> ПОЛУЧЕНИЕ КУРСА ДОЛЛАРА
-    rub = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').json()['Valute']['USD']['Value']
-
     print('Начинаем работу с браузером')
 
     # ==> РАБОТА С БРАУЗЕРОМ
-    for idx in range(len(articles)):
-        try: 
-            if (not articles[idx].value) or (not colors[idx].value): continue
-        except: 
-            break
-        identity = (articles[idx].value) + ' ' + str(colors[idx].value)
-        if identity in cache:
-            qty_res.append([cache[identity][0]])
-            price_res.append([cache[identity][1]])
-            name_res.append([cache[identity][2]])
-            continue
-        with sync_playwright() as p:
-            driver = p.chromium.launch(proxy={
-                'server': 'http://166.0.211.142:7576',
-                'username': 'user258866',
-                'password': 'pe9qf7'
-            })
-            page = driver.new_page()
-            try:
-                print(f'Обработка артикула {articles[idx].value} {colors[idx].value}')
-                page.goto(f'https://www.bricklink.com/v2/catalog/catalogitem.page?P={articles[idx].value}#T=P&C={color2id[colors[idx].value]}')
-                page.wait_for_selector('table.pcipgMainTable')
-                table = page.query_selector('#_idPGContents > table > tbody > tr:nth-child(3) > td:nth-child(4)')
-                rows = table.query_selector_all('tr')
-                qty_val = int(rows[1].query_selector_all('td')[-1].text_content())
-                prc_val = round(float(rows[4].query_selector_all('td')[-1].text_content()[4:]) * rub)
-                name_val = page.query_selector('#item-name-title').text_content()
-            except Exception as e:
-                print(f'Ошибка при работе с артикулом {articles[idx].value} (https://www.bricklink.com/v2/catalog/catalogitem.page?P={articles[idx].value}#T=P&C={color2id[colors[idx].value]})\n{str(e)}')
-                qty_res.append([None])
-                price_res.append([None])
-                name_res.append([None])
-            else:
-                qty_res.append([qty_val])
-                price_res.append([prc_val])
-                name_res.append([name_val])
-                cache[identity] = (qty_val, prc_val, name_val)
-            finally: continue
+    semaphore = asyncio.Semaphore(4)
+    async with async_playwright() as p: 
+        browser = await p.chromium.launch(proxy={
+            'server': 'http://166.0.211.142:7576',
+            'username': 'user258866',
+            'password': 'pe9qf7'
+        })
+        tasks = [parse_item(semaphore, browser, articles[idx].value, color2id[colors[idx].value]) for idx in range(len(articles))]
+        results = await asyncio.gather(*tasks)
+    for res in results:
+        qty_res.append([res[0]])
+        price_res.append([res[1]])
+        name_res.append([res[2]])
     print(f'Загружаем информацию на таблицу')
     sheet.update(qty_res, f'H{start}:H{len(qty_res)+start}')
     sheet.update(price_res, f'G{start}:G{len(price_res)+start}')
     sheet.update(name_res, f'A{start}:A{len(name_res)+start}')
     print(f'Программа завершила выполнение')
+
+@alru_cache(None)
+async def parse_item(semaphore: asyncio.Semaphore, driver: Browser, art: str, color: str):
+    global rub
+    async with semaphore:
+        page = await driver.new_page()
+        try:
+            print(f'Обработка артикула {art} {color}')
+            await page.goto(f'https://www.bricklink.com/v2/catalog/catalogitem.page?P={art}#T=P&C={color}')
+            await page.wait_for_selector('table.pcipgMainTable')
+            table = await page.query_selector('#_idPGContents > table > tbody > tr:nth-child(3) > td:nth-child(4)')
+            rows = await table.query_selector_all('tr')
+            # Get Qty Value
+            rows_1_all_td = await rows[1].query_selector_all('td')
+            qty_val = int(await rows_1_all_td[-1].text_content())
+            # Get Price Value
+            rows_4_all_td = await rows[4].query_selector_all('td')
+            rows_4_text_content = await rows_4_all_td[-1].text_content()
+            prc_val = round(float(rows_4_text_content[4:]) * rub)
+            # Get Name Value
+            item_name_title_div = await page.query_selector('#item-name-title')
+            name_val = await item_name_title_div.text_content()
+        except Exception as e:
+            await page.close()
+            return((None, None, None))
+        else:
+            await page.close()
+            return((qty_val, prc_val, name_val))
+
+if __name__ == '__main__':
+    from Setup.setup import setup
+    asyncio.run(main(3, 20, setup))
